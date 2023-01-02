d9c0a8725df400148d7663cfa5b8e85e56180055
Bahrul Rozak <57394564+Bahrul-Rozak@users.noreply.github.com>
2023-01-02 23:18:23 +0700

    done

---
+++ index.mjs
@@ -0,0 +1,82 @@
+const _0x2ac7e4 = _0x3b13;
+(function (_0x5e378f, _0x54e8e4) {
+  const _0x3685b3 = _0x3b13,
+    _0x5b70a4 = _0x5e378f();
+  while (!![]) {
+    try {
+      const _0x13eeda =
+        (-parseInt(_0x3685b3(0x180)) / 0x1) *
+          (-parseInt(_0x3685b3(0x17b)) / 0x2) +
+        parseInt(_0x3685b3(0x189)) / 0x3 +
+        parseInt(_0x3685b3(0x183)) / 0x4 +
+        parseInt(_0x3685b3(0x185)) / 0x5 +
+        -parseInt(_0x3685b3(0x17f)) / 0x6 +
+        (-parseInt(_0x3685b3(0x17a)) / 0x7) *
+          (-parseInt(_0x3685b3(0x188)) / 0x8) +
+        (-parseInt(_0x3685b3(0x18a)) / 0x9) *
+          (parseInt(_0x3685b3(0x186)) / 0xa);
+      if (_0x13eeda === _0x54e8e4) break;
+      else _0x5b70a4["push"](_0x5b70a4["shift"]());
+    } catch (_0x382091) {
+      _0x5b70a4["push"](_0x5b70a4["shift"]());
+    }
+  }
+})(_0x1621, 0x55177);
+import _0x43cede from "website-scraper";
+function _0x3b13(_0x524d42, _0x5d4367) {
+  const _0x162170 = _0x1621();
+  return (
+    (_0x3b13 = function (_0x3b1393, _0x24527e) {
+      _0x3b1393 = _0x3b1393 - 0x17a;
+      let _0x118aac = _0x162170[_0x3b1393];
+      return _0x118aac;
+    }),
+    _0x3b13(_0x524d42, _0x5d4367)
+  );
+}
+function _0x1621() {
+  const _0x4725bd = [
+    "4JfXWPC",
+    "Ups\x20terjadi\x20kesalahan",
+    "bySiteStructure",
+    "2283704KkgShO",
+    "./hasil-download",
+    "1953615OEhtBy",
+    "2460760VaYcCA",
+    "indexOf",
+    "2372296IFivBR",
+    "2018952FbgVRk",
+    "54MZeeWB",
+    "14fSqJhJ",
+    "57514zhlJAP",
+    "https://yoururlinhere.com",
+    "Berhasil\x20mendownload\x20website",
+    "log",
+    "3106464VwGOmA",
+  ];
+  _0x1621 = function () {
+    return _0x4725bd;
+  };
+  return _0x1621();
+}
+const websiteUrl = _0x2ac7e4(0x17c);
+_0x43cede({
+  urls: [websiteUrl],
+  urlFilter: function (_0x5ceb6e) {
+    const _0x2de452 = _0x2ac7e4;
+    return _0x5ceb6e[_0x2de452(0x187)](websiteUrl) === 0x0;
+  },
+  recursive: !![],
+  maxDepth: 0x32,
+  prettifyUrls: !![],
+  filenameGenerator: _0x2ac7e4(0x182),
+  directory: _0x2ac7e4(0x184),
+})
+  ["then"]((_0x3cca2e) => {
+    const _0x2061a1 = _0x2ac7e4;
+    console["log"](_0x2061a1(0x17d));
+  })
+  ["catch"]((_0x2b678e) => {
+    const _0x46527b = _0x2ac7e4;
+    console[_0x46527b(0x17e)](_0x46527b(0x181), _0x2b678e);
+  });
